c24ac9d0e3d00d6228f0cfc227de069e7e752b84
muradkerimov <muradik@code.edu.az>
2021-10-16 06:34:01 +0400

    Add project files.

---
+++ HomeWork-10-16(Task-1)/Advertisement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HomeWork_10_16_Task_1_
+{
+    class Advertisement : IComparable
+    {
+        public string AddName { get; set; }
+        public double Price { get; set; }
+        public string OwnerAdd { get; set; }
+
+        public Category category;
+
+        public Advertisement()
+        {
+
+        }
+
+        public Advertisement(string addName, double price, string ownerAdd)
+        {
+            AddName = addName;
+            Price = price;
+            OwnerAdd = ownerAdd;
+        }
+
+        public override string ToString()
+        {
+            return $"{AddName} {Price} {OwnerAdd}";
+        }
+
+        public static bool operator <(Advertisement ad1, Advertisement ad2)
+        {
+            return ad1.Price < ad2.Price;
+        }
+
+        public static bool operator >(Advertisement ad1, Advertisement ad2)
+        {
+            return ad1.Price > ad2.Price;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return Price.CompareTo(((Advertisement)obj).Price);
+        }
+    }
+}
---
+++ HomeWork-10-16(Task-1)/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace HomeWork_10_16_Task_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Advertisement add1 = new Advertisement("addName1", 32.51, "ownerAdd1");
+            Advertisement add2 = new Advertisement("addName2", 22.52, "ownerAdd2");
+            Advertisement add3 = new Advertisement("addName3", 12.53, "ownerAdd3");
+
+            Advertisement[] advertisements = { add1, add2, add3 };
+            try
+            {
+                Array.Sort(advertisements);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Gonderdiyin array sort oluna bilmir.");
+            }
+
+            foreach (Advertisement item in advertisements)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.Write("1 ile 5 arasinda eded daxil edin:");
+            int enums = Convert.ToInt32(Console.ReadLine());
+            Advertisement add = new Advertisement();
+            add.category = (Category)enums;
+
+            if (add.category == Category.Preschoolers)
+            {
+                Console.WriteLine("everyone can watch");
+            }
+            else if (add.category == Category.Kids)
+            {
+                Console.WriteLine("3+ can watch");
+            }
+            else if (add.category == Category.Tweens)
+            {
+                Console.WriteLine("7+ can watch");
+            }
+            else if (add.category == Category.Teens)
+            {
+                Console.WriteLine("13+ can watch");
+            }
+            else
+            {
+                Console.WriteLine("18+ can watch");
+            }
+        }
+    }
+    enum Category : byte{Preschoolers = 1, Kids, Tweens, Teens, Adults}
+}
